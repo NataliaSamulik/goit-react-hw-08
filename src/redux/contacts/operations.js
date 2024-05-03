608d16791e6afa856ca9a3d87a4d47a8c3f3e380
NataliaSamulik <107351260+NataliaSamulik@users.noreply.github.com>
2024-05-03 16:57:05 +0300

    start

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await axios.get('/contacts');
+      return resp.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async ({ name, number }, thunkAPI) => {
+    try {
+      const resp = await axios.post('/contacts', {
+        name: name,
+        number: number,
+      });
+      return resp.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contact/deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      const resp = await axios.delete(`/contacts/${id}`);
+      return resp.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
